5de250ed20fc0f363183e6f1b17a951aa4f472d2
Jonathan Noguerales <jonathan.n@pyv.es>
2024-10-04 14:10:47 +0200

    Cambios creación componentes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screens: {
+        1440: "1440px",
+        1920: "1920px",
+      },
+    },
+  },
+  plugins: [],
+};
